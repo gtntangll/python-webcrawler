8064ac594d04ddba0407467b22b2245dc6fe6f42
Your Name <you@example.com>
2017-10-20 15:01:25 +0800

    10/20

---
+++ demo/demo2.py
@@ -0,0 +1,115 @@
+# -*- coding:utf-8 -*-
+import urllib
+import urllib2
+import re
+
+class Tool:
+    removeImg = re.compile('<img.*?>| {7}|')
+    removeAddr = re.compile('<a.*?>|</a>')
+    replaceLine = re.compile('<tr>|<div>|</div>|</p>')
+    replaceTD = re.compile('<td>')
+    replacePara = re.compile('<p.*?>')
+    replaceBR = re.compile('<p.*?>')
+    replaceBR = re.compile('<br><br>|<br>')
+    removeExtraTag = re.compile('<.*?')
+    def replace(self,x):
+        x = re.sub(self.removeImg,"",x)
+        x = re.sub(self.removeAddr,"",x)
+        x = re.sub(self.replaceLine,"\n",x)
+        x = re.sub(self.replaceTD,"\t",x)
+        x = re.sub(self.replacePara,"\n     ",x)
+        x = re.sub(self.replaceBR,"\n",x)
+        x = re.sub(self.removeExtraTag,"",x)
+        
+        return x.strip()
+
+class DEMO:
+    
+    def __init__(self,baseUrl,seeLZ):
+        
+        self.baseURL = baseUrl
+        self.seeLZ = '?see_lz=' +str(seeLZ)
+        self.tool = Tool()
+
+    def getPage(self,pageNum):
+        
+        try:
+            url = self.baseURL+self.seeLZ + '&pn=' + str(pageNum)
+            request = urllib2.Request(url)
+            response = urllib2.urlopen(request)
+            return response.read().decode('utf-8')
+        except urllib2.URLError, e:
+            if hasattr(e,"reson"):
+                print u"链接失败,原因:",e.reason
+                return None
+
+    def getTitle(self,title):
+        
+        page = self.getPage(1)
+        pattern = re.compile('<h3 class="core_title_txt.*?>(.*?)</h3>',re.S)
+        result = re.search(pattern,page)
+        if result:
+            return result.group(1).strip()
+        else:
+            return None
+
+    def getPageNum(self,page):
+        
+        page = self.getPage(1)
+        pattern = re.compile('<li class="l_reply_num".*?</span>.*?<span.*?>(.*?)</span>',re.S)
+        result = re.search(pattern,page)
+        if result:
+            return result.group(1).strip()
+        else:
+            return None
+
+    def getContent(self,page):
+        
+        pattern = re.compile('<div id="post_content_.*?>(.*?)</div>',re.S)
+        items = re.findall(pattern,page)
+        
+        contents = []
+        for item in items:
+            content = "\n" + self.tool.replace(item) + '\n'
+            contents.append(content.encode('utf-8'))
+        return contents
+
+    def setFileTitle(self,title):
+        
+        if title is not None:
+            self.file = open(title + ".txt","w+")    
+        else:
+            self.file = open(self.defaultTitle + ".txt","w+")
+        
+    def writeData(self,contents):
+        
+        for item in contents:
+            self.file.write(item) 
+
+    def start(self):
+        indexPage = self.getPage(1)
+        pageNum = self.getPageNum(indexPage)
+        title = self.getTitle(indexPage)
+        self.setFileTitle(title)
+        if pageNum == None:
+            print "URL已失效,请重试"
+            return None
+        try:
+            print "该帖子共有" + str(pageNum) + "页"
+            for i in range(1,int(pageNum)+1):
+                print "正在写入第" + str(i) + "页数据"
+                page = self.getPage(i)
+                contents = self.getContent(page)
+                self.writeData(contents)
+        
+        except IOError,e:
+            print "写入异常,原因" + e.message
+
+        finally:
+            print "写入任务完成"
+
+print u"请输入帖子代号"
+baseURL = 'http://tieba.baidu.com/p/' + str(raw_input(u'http://tieba.baidu.com/p/'))
+seeLZ = raw_input("是否只获取楼主发言,是输入1,否输入0\n")
+demo = DEMO(baseURL,seeLZ)
+demo.start()
